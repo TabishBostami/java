d6eea62b892801ac77d7f7e86e49f410c9a428f5
TabishBostami <143376901+TabishBostami@users.noreply.github.com>
2023-09-24 18:03:22 +0530

    Add files via upload

---
+++ inverse_halfpyramid.java
@@ -0,0 +1,16 @@
+public class inverse_halfpyramid {
+    public static void main(String[] args) {
+        
+        // int m=5;
+        
+
+     for(int i=5;i>0;i--){
+        for(int j=1;j<i;j++){
+            System.out.print("* ");
+        }
+        System.out.println(" ");
+     }
+    }
+}
+    
+
