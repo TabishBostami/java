fbd400696a17982b9f743c85f8ee3e5e4ebcad8a
TabishBostami <143376901+TabishBostami@users.noreply.github.com>
2023-09-26 18:14:19 +0530

    Add files via upload

---
+++ solid_rhombus.java
@@ -0,0 +1,18 @@
+public class solid_rhombus {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int k =1;k<=n;k++){
+
+        
+        for(int i=1;i<(2*n)-k;i++){
+            System.out.print(" ");
+        }
+        for(int j=0;j<n;j++){
+            System.out.print("*");
+        }
+
+        System.out.println("");
+    }
+    
+  }
+}
